8b2926efdabfae25a41675c5d5a5c3abcff52a25
Dishaajh <dishaajh@gmail.com>
2025-01-16 11:29:13 +0530

    upload images

--- src/components/section5/Properties.jsx
+++ src/components/section5/Properties.jsx
@@ -4,17 +4,17 @@ import "./Properties.css";
 const Properties = () => {
   const properties = [
     {
-      image: "/public/citywise1.jpg", 
+      image: "/Home-Page/citywise1.jpg", 
       title: "Palm Vista, Mumbai",
       description: "Master community within 16 acres of tropical paradise.",
     },
     {
-      image: "/public/citywise2.jpg",
+      image: "/Home-Page/citywise2.jpg",
       title: "Winworth, Kochi",
       description: "Eddappally’s Finest Lifestyle for Timeless Living",
     },
     {
-      image: "/public/citywise3.jpg",
+      image: "/Home-Page/citywise3.jpg",
       title: "Capella, Bangalore",
       description:
         "World class integrated lifestyle, moments away from everything",
